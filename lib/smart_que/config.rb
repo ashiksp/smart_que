08dc36a39e1c75ded9c33420d15ba287572991dc
Ashik Salman <ashiksp@gmail.com>
2017-08-01 14:41:04 +0530

    Allow configurations for smart que module.

---
+++ lib/smart_que/config.rb
@@ -0,0 +1,48 @@
+module SmartQue
+  class Config
+    # Configurations
+    # allowed configurations
+    CONFIGURATION = %w(host port queues env username password logfile logger)
+
+    # Initialize
+    def initialize
+      @config = {}
+      load_configuration_from_yml
+    end
+
+    def method_missing(name, *args)
+      setter = false
+
+      name = name.to_s
+
+      if name =~ /=$/
+        name = name.to_s.chop
+        setter = true
+      end
+
+      super(name, args) unless CONFIGURATION.include?(name)
+
+      if setter
+        set(name, args.first)
+      else
+        get(name)
+      end
+    end
+
+    private
+
+    def load_configuration_from_yml
+      if File.exists? 'config/smart_que.yml'
+        @config = YAML.load(File.read('config/smart_que.yml')) || @config
+      end
+    end
+
+    def set(key, val)
+      @config[key] = val
+    end
+
+    def get(key)
+      @config[key]
+    end
+  end
+end
