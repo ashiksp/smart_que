c3cabfd583acca2c879fea6e3956269231dc025c
Ashik Salman <ashiksp@gmail.com>
2017-08-01 15:17:44 +0530

    Add Publisher class implementation for SmartQue.

---
+++ lib/smart_que/publisher.rb
@@ -0,0 +1,34 @@
+module SmartQue
+  class Publisher < Publisher::Base
+    
+    # Initialize
+    def initialize
+      @channel = channel
+      queue_list.each do |q_name|
+        q = get_queue(q_name, @channel)
+        q.bind(x_direct, routing_key: q.name)
+      end
+    end
+
+    # Instance methods
+
+    # Publish message to the respective queue
+    def publish(queue, options = {})
+      # Check queue name includes in the configured list
+      # Return if queue doesn't exist
+      if queue_list.include? queue
+        # Publish sms to queue
+        x_direct.publish(
+                          options.to_json,
+                          mandatory: true,
+                          routing_key: get_queue(queue).name
+                        )
+        log_message("Publish status: success, Queue : #{queue}, Content : #{options}")
+      else
+        log_message("Publish status: failed, Queue(#{queue}) doesn't exist.")
+        log_message("Content : #{options}")
+        raise QueueNotFoundError
+      end
+    end
+  end
+end
