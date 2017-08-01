2af143491e32b4fa8ac1b3069cd6f48f52b7c543
Ashik Salman <ashiksp@gmail.com>
2017-08-01 14:53:33 +0530

    Import configurations to SmartQue module.

--- lib/smart_que.rb
+++ lib/smart_que.rb
@@ -1,5 +1,19 @@
 require "smart_que/version"
+require "smart_que/config"
+
+require "bunny"
+require "yaml"
+require "json"
+require "logger"
 
 module SmartQue
-  # Your code goes here...
+  # Methods related to configurations
+  def self.config
+    @config ||= Config.new
+  end
+
+  def self.configure
+    yield(config) if block_given?
+    config
+  end
 end
