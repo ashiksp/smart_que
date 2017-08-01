4fbfa0dc909c20a08fea4f78fbb047ba9d4c1395
Ashik Salman <ashiksp@gmail.com>
2017-08-01 15:11:48 +0530

    Add base utilities for publisher class.

---
+++ lib/smart_que/publisher/base.rb
@@ -0,0 +1,42 @@
+module SmartQue
+  module Publisher
+    class Base
+      # List Queues from configuration
+      def queue_list
+        @queue_list ||= ::SmartQue.config.queues
+      end
+
+      # Methods related to bunny exchange, channels, queues
+      def channel
+        @channel ||= connection.create_channel
+      end
+
+      # Direct exchange
+      def x_direct
+        @x_direct ||= channel.direct("smartque.direct")
+      end
+
+      # Connection Object
+      def connection
+        ::SmartQue.establish_connection
+      end
+
+      # Get/Set queue with name
+      # name : sms_otp
+      def get_queue(q_name)
+        channel.queue(modified_q_name(q_name))
+      end
+
+      # Logging
+      def log_message(data)
+        ::SmartQue.log(data)
+      end
+
+      private
+
+      def modified_q_name(q_name)
+        q_name.downcase.sub('_','.')
+      end
+    end
+  end
+end
